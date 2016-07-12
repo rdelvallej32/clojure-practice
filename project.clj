e7b21e791fed0409cef1ef7859beeb8a956f3a58
rdelvallej32 <rdelvallej@gmail.com>
2016-07-11 20:33:02 -0400

    Add dependencies and main namespace
    - Add two dependencies, ring-core and ring-jetty-adapter
    - The ring-core provides fundamental functions to handle HTTP request/response
    - ring-jetty-adapter provides the web server feature by Jetty
    - Add a main function,

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                  [ring/ring-core "1.2.1"]
+                  [ring/ring-jetty-adapter "1.2.1"]]
+  :main hello-ring.core)
